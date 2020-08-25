e8a765d327e4fb8cfac3675c19e2253506c55016
小林亮介 <kobayashiryousuke@kobayashiryousukenoMacBook-Pro.local>
2020-08-25 13:46:59 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"app/samples"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(samples.Fib(10))
+}
---
+++ samples/sample_test.go
@@ -0,0 +1,12 @@
+package samples
+
+import "testing"
+
+func TestFib(t *testing.T) {
+	result := Fib(8)
+	expected := 21
+
+	if result != expected {
+		t.Fatal("failed")
+	}
+}
